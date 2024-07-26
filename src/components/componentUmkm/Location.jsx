006bd949108fb12589240684c3b1770a3e586a85
unknown <muizzuddin334@gmail.com>
2024-07-26 12:46:50 +0700

    update btn and fix bug location

--- src/components/componentUmkm/Location.jsx
+++ src/components/componentUmkm/Location.jsx
@@ -1,12 +1,21 @@
 /* eslint-disable react/prop-types */
 const Location = ({ location }) => {
-  return location ? (
+  const isValidURL = (location) => {
+    try {
+      new URL(location);
+      return location;
+    } catch (err) {
+      return "";
+    }
+  };
+
+  return isValidURL(location) ? (
     <section className="max-w-screen-lg px-5 mx-auto mb-20">
       <p className="text-center mb-4 text-3xl font-extrabold tracking-tight leading-none md:text-4xl xl:text-5xl ">
         Lokasi Kami
       </p>
       <iframe
-        src={location || ""}
+        src={location}
         className="w-full h-[50vh] md:h-[60vh] lg:h-[80vh] rounded-md"
         allowFullScreen
         loading="lazy"
